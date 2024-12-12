195814e61ac3b5d0efc0f36f6d59600943f60184
bwondessen <bwondessen23@gmail.com>
2024-12-11 18:54:07 -0600

    Initial commit

---
+++ RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesApp.swift
+//  Recipes
+//
+//  Created by Bruke Wondessen on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
