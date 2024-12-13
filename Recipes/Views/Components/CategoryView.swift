bc7ff49682a626ce97b31e3944a0308260fbb3db
bwondessen <bwondessen23@gmail.com>
2024-12-12 23:23:38 -0600

    Implement filtering by categories

---
+++ Recipes/Views/Components/CategoryView.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryView.swift
+//  Recipes
+//
+//  Created by Bruke Wondessen on 12/12/24.
+//
+
+import SwiftUI
+
+struct CategoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryView()
+}
